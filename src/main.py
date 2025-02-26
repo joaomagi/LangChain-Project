a259e9192a975f29f32f0bf0d55512ad9e40742f
joaomagi <joaofrancisco.leaomagi@outlook.com>
2025-02-26 11:10:57 -0300

    new file:   src/__init__.py 	modified:   src/main.py 	new file:   src/model.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from dotenv import load_dotenv # Biblioteca para utilização do dotenv
 import os # Biblioteca para utilização do dotenv
-import re # Biblioteca para validar se uma string contem um caracter
 from langchain_groq import ChatGroq # Biblioteca para utilização do grop
-import json # Biblioteca para conversar para json
+
 from langchain_core.runnables import RunnableLambda ## para transformar as funções em etapas para chain aceitar sem erro com |
+
+from model import validate_question
+from model import processing_questions
+from model import virtual_teacher
 
 # Salva a chave api em uma variavel e carrega ela e verificar se está valida
 load_dotenv()
@@ -18,37 +21,6 @@ groq = ChatGroq(
     max_retries=2,
 )
 
-
-
-# Criação de um modelo que contenha algumas expressões matemáticas para validação
-math_expressions =  r"[0-9+\-×÷=]|\b(soma|subtração|multiplicação|divisão|igual|)\b"
-
-
-# Validação aperenta estar correta necessita de mais alguns testes
-def validate_question(question: str):
-        while True:
-            if not re.search(math_expressions,question, re.IGNORECASE): #IGNORECASE
-               print("Tente uma pergunta que contenha números ou expressões matemáticas")
-               # Tratar o "erro"
-            else:
-                return question
-        
-def processing_questions(question):    
-    question_dict = {
-        "question" : question,
-        "category" : "math",
-    }
-
-    question_json = json.dumps(question_dict)
-    return question_json
-
-
-def virtual_teacher(json_question):
-    prompt = [
-    ("system", "You are a math-focused AI. Your goal is to answer math problems, in a simple way, with step-by-step resolutions, clear explanations, in Brazilian Portuguese."),
-    ("human", json_question),
-    ]
-    return prompt
 
 if __name__ == "__main__":
 
---
+++ src/model.py
@@ -0,0 +1,32 @@
+import json # Biblioteca para conversar para json
+import re # Biblioteca para validar se uma string contem um caracter
+
+
+# Criação de um modelo que contenha algumas expressões matemáticas para validação
+math_expressions =  r"[0-9+\-×÷=]|\b(soma|subtração|multiplicação|divisão|igual|)\b"
+
+
+# Validação aperenta estar correta necessita de mais alguns testes
+def validate_question(question: str):
+        while True:
+            if not re.search(math_expressions,question, re.IGNORECASE): #IGNORECASE
+               print("Tente uma pergunta que contenha números ou expressões matemáticas")
+            else:
+                return question
+        
+def processing_questions(question):    
+    question_dict = {
+        "question" : question,
+        "category" : "math",
+    }
+
+    question_json = json.dumps(question_dict)
+    return question_json
+
+
+def virtual_teacher(json_question):
+    prompt = [
+    ("system", "You are a math-focused AI. Your goal is to answer math problems, in a simple way, with step-by-step resolutions, clear explanations, in Brazilian Portuguese."),
+    ("human", json_question),
+    ]
+    return prompt
