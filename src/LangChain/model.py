55a153e8326eba1e0e844a9f253ae5af503a26e6
joaomagi <joaofrancisco.leaomagi@outlook.com>
2025-02-26 21:59:41 -0300

    Versão teste

--- src/LangChain/model.py
+++ src/LangChain/model.py
@@ -10,22 +10,16 @@ math_expressions =  r"[0-9+\-×÷=]|\b(soma|subtração|multiplicação|divisão
 def validate_question(question: str):
     if not re.search(math_expressions,question, re.IGNORECASE):
        raise ValueError("A pergunta não é sobre matemática")  # Retorna uma exceção caso a pergunta não seja válida
-    else:
-        return question
+    return question
 
 # Função para processar e converter a perunta para o formato JSON
-def processing_questions(question):    
-    question_dict = {
-        "question" : question, # Define a perunta
-        "category" : "math", # Define de forma fixa a categoria como matemática
-    }
-
-    question_json = json.dumps(question_dict) # Converte o dicionário para JSON
-    return question_json
+def processing_questions(question: str):    
+    return json.dumps({"question": question, "category":"math"}) # Formata a pergunta como JSON, incluindo a categoria 'math'
+  
 
 
 # Função que estrutura o prompt para um assistente virtual de matemática (Professor virtual)
-def virtual_teacher(json_question):
+def virtual_teacher(json_question: str):
     prompt = [
         ("system", "You are a math-focused AI. Your goal is to answer math problems, in a simple way, with step-by-step resolutions, clear explanations, in Brazilian Portuguese."),
         ("human", json_question), # Adiciona a pergunta formatada no prompt
